3b1247a587ebecdb9f8ca0decd006f1616e07884
Edgard Barbosa <edgard.barbosa@cortex-intelligence.com>
2019-01-09 18:57:36 -0200

    09 Ciclo de vida do Ionic

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,44 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, LoadingController, AlertController } from 'ionic-angular';
+import { Carro } from '../../models/carro';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http'
+import { CarrosServiceProvider } from '../../providers/carros-service/carros-service';
+import { NavLifecycles } from '../../utils/ionic/nav/nav-lifecycles';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
+export class HomePage implements NavLifecycles {
 
-  constructor(public navCtrl: NavController) {
+  public carros: Carro[];
 
+  constructor(public navCtrl: NavController, 
+    private _http: HttpClient, 
+    private _loadingCtrl: LoadingController,
+    private _alertCtrl: AlertController,
+    private _carrosService: CarrosServiceProvider) {
   }
 
+  ionViewDidLoad() {
+    const loading = this._loadingCtrl.create({ content: 'Carregando...' });
+
+    loading.present();
+  
+    this._carrosService.getAll()
+      .subscribe(
+        (carros) => {
+          this.carros = carros;
+          loading.dismiss();
+      }, (err: HttpErrorResponse) => {
+          loading.dismiss();
+          this._alertCtrl.create( { 
+            title:'Connection Error', 
+            subTitle: 'Não foi possível carregar a lista de carros',
+            buttons: [
+              { text: 'ok' }
+            ]}).present();
+      });
+
+  }
 }
